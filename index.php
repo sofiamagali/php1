2ec7388372f4409cce85eb61c761b0d3d8257020
Sofi <69173661+sofiamagali@users.noreply.github.com>
2021-07-05 11:50:36 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$nombre = "sofia";
+$apellido = "fuhrmann";
+$edad = 23;
+$hobbie= "nadar";
+$editor = "visualStudio";
+
+
+echo $nombre,$apellido,$edad,$hobbie,$editor,PHP_OS;
+
+
+
+
+
+?>
